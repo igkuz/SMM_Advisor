e4b46c4e4d9b88c79b42f43c76f3cbf14651fef9
Igor Kuznetsov <igkuznetsov@gmail.com>
2013-09-14 10:28:36 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SMMAdvisor::Application.config.secret_key_base = '4d44e7c0cbff9ec08f9508feee5c5f5db2f46b7d7c17f7df039f45a2f01fd4ce35b43b2d248205b7edae65d18bb873b5c8feee3d36c31a67e84179f8af3767ec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SMMAdvisor::Application.config.session_store :cookie_store, key: '_SMM_Advisor_session'
